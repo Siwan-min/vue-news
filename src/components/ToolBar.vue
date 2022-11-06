7afdb7fedac01a5442bba77468232d9f67e5cc34
Siwan-min <quddlf1302@gmail.com>
2022-11-06 23:30:07 +0900

    ignore commit

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> | 
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: #42b883;
+  color: white;
+  padding: 8px 8px 8px 25px;
+  display: flex;
+  align-items: center;
+}
+
+.header .router-link-exact-active {
+    color: #35495e;
+}
+
+.header a {
+    color: white;
+}
+
+</style>
