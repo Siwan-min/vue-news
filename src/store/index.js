46b0c5761bafdd83b1d62b2b654b16d4b251d2d0
Siwan-min <quddlf1302@gmail.com>
2022-11-10 08:49:51 +0900

    mutation, actions 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,42 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList, fetchJobsList } from '../api/index.js';
+
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        news: []
+        news: [],
+        jobs: [],
+    },
+    mutations: {
+        SET_NEWS(state, news) {
+            state.news = news;
+        },
+        SET_JOBS(state, jobs) {
+            state.jobs = jobs;
+        }
+    },
+    actions: {
+        FETCH_NEWS(context) {
+            fetchNewsList()
+                .then( response => {
+                    console.log(response.data);
+                    context.commit('SET_NEWS', response.data);
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+        },
+        FETCH_JOBS(context) {
+            fetchJobsList()
+                .then( response => {
+                    context.commit('SET_JOBS', response.data);
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+        }
     }
 });
