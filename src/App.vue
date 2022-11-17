74cfdd713df7dbf15fcef2217449a3fa42ef1cab
Siwan-min <quddlf1302@gmail.com>
2022-11-17 19:47:53 +0900

    Router Transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <transition>
+    <transition name="page">
       <router-view></router-view>
     </transition>
   </div>
@@ -21,5 +21,13 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
